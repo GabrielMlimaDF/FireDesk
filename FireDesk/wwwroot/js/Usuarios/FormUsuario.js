52f64380db80520d3cf27142727808740d2787d0
Gabriel Matos <gabrieldevbrasilia@gmail.com>
2024-09-16 10:06:04 -0300

    CSS tela de cad de usuario

---
+++ FireDesk/wwwroot/js/Usuarios/FormUsuario.js
@@ -0,0 +1,9 @@
+﻿function mostrarModal() {
+    document.getElementById("modalOverlay").style.display = "block";
+    document.getElementById("modal").style.display = "flex";
+   
+}
+function fecharModal() {
+    document.getElementById("modalOverlay").style.display = "none";
+    document.getElementById("modal").style.display = "none";
+}
