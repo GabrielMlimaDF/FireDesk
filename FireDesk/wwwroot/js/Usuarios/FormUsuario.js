0de45cb42d63f74180cd1e8c07c5218c48bd61eb
Gabriel Matos <gabrieldevbrasilia@gmail.com>
2024-10-09 17:14:02 -0300

    Incluindo select do tipo de usuario

--- FireDesk/wwwroot/js/Usuarios/FormUsuario.js
+++ FireDesk/wwwroot/js/Usuarios/FormUsuario.js
@@ -1,9 +1,15 @@
 ﻿function mostrarModal() {
     document.getElementById("modalOverlay").style.display = "block";
     document.getElementById("modal").style.display = "flex";
-   
 }
 function fecharModal() {
     document.getElementById("modalOverlay").style.display = "none";
     document.getElementById("modal").style.display = "none";
 }
+const btnCheckbox = document.getElementById('foo');
+const statusSpan = document.getElementById('ativoInativo');
+function statusAtivoInativo() {
+    const checkbox = document.getElementById('foo');
+    checkbox.checked ? statusSpan.textContent = "Ativo" : statusSpan.textContent = "Inativo";
+}
+btnCheckbox.addEventListener("click", statusAtivoInativo);
---
+++ FireDesk/wwwroot/js/ValidarCpf/validarCpf.js
@@ -0,0 +1,35 @@
+﻿function mascaraCPF(cpf) {
+    // Remove caracteres não numéricos
+    cpf.value = cpf.value.replace(/\D/g, '');
+
+    // Aplica a máscara
+    cpf.value = cpf.value.replace(/(\d{3})(\d)/, '$1.$2');
+    cpf.value = cpf.value.replace(/(\d{3})(\d)/, '$1.$2');
+    cpf.value = cpf.value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+}
+
+function calcularDigito(cpf, peso) {
+    let soma = 0;
+    for (let i = 0; i < peso; i++) {
+        soma += Number(cpf[i]) * (peso + 1 - i);
+    }
+    const resto = (soma * 10) % 11;
+    return resto === 10 ? 0 : resto;
+}
+
+function validarCPF(cpf) {
+    const regex = /^(?!0{11})(\d{3}\.\d{3}\.\d{3}-\d{2})$/;
+    if (!regex.test(cpf)) return false;
+
+    cpf = cpf.replace(/\D/g, '');
+    const digito1 = calcularDigito(cpf, 9);
+    const digito2 = calcularDigito(cpf, 10);
+    return cpf[9] == digito1 && cpf[10] == digito2;
+}
+document.getElementById('cpf').addEventListener('blur', function () {
+    if (!validarCPF(this.value)) {
+        document.getElementById('mensagem').innerText = 'CPF inválido.';
+    } else {
+        document.getElementById('mensagem').innerText = 'CPF válido!';
+    }
+});
